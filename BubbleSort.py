f60ba3d36f20b6a3741e75993968385c4d428b88
Milad Nemati <mnem0001@student.monash.edu>
2021-10-25 13:07:10 +1100

    bubble sort

---
+++ BubbleSort.py
@@ -0,0 +1,16 @@
+arrayToSort = [2, 3, 4,1]
+
+
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range((len(array)-1 - i)):
+            n = array[j + 1]
+            p = array[j]
+            if p > n:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j+1] = temp
+    print(array)
+
+
+bubbleSort(arrayToSort)
